a06f5787df20c1963afa1f2ff9bb42eee99a0e93
Jason Storey <jason.storey@unrulymedia.com>
2014-04-01 19:19:45 +0100

    Grab some MIDI parsing code internetz... must study

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-var $ = require('jquery');
+var $ = require('jquery'),
+	MidiFile = require('./midi-parser.js');
+
 
 var request = $.ajax({
 	url: './Claude Debussy - Claire De Lune.mid',
 	dataType: 'text',
-	type: 'GET'
+	mimeType: 'text/plain; charset=x-user-defined',
+	type: 'GET',
+	processData: false
 });
 
-request.done(function(midiFile){
+request.done(function(dataString) {
+	var midiData = dataString || "",
+		ff = [];
+
+	for (var i = 0; i < midiData.length; i++) {
+		ff[i] = String.fromCharCode(midiData.charCodeAt(i) & 255);
+	}
+
+	var midiFile = MidiFile(ff.join(""));
 	console.log(midiFile);
 });
 
